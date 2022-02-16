497fb0a39236f34c9b025babe1a9c5ca5a4ccaab
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2022-02-16 11:25:19 +0300

    Update script.js

--- Recipe App/script.js
+++ Recipe App/script.js
@@ -3,7 +3,6 @@ const favoriteContainer = document.getElementById("fav-meals");
 const mealPopup = document.getElementById("meal-popup");
 const mealInfoEl = document.getElementById("meal-info");
 const popupCloseBtn = document.getElementById("close-popup");
-
 const searchTerm = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
 
